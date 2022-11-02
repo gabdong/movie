ad3f9c9f37ccc28bc3d161aaa6cebe85ab1c8293
gabdong <hwan970104@gmail.com>
2022-11-02 21:40:39 +0900

    React deploy

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hello" element={<h1>hello</h1>} />
-        <Route path="/movie/:id" element={<Detail />} />
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/hello"} element={<h1>hello</h1>} />
+        <Route path={"/movie/:id"} element={<Detail />} />
       </Routes>
     </Router>
   );
